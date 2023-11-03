0eb0951ee77e2b6e1152d62751546df0112900ba
Courtney Cho <courtneycho@Courtneys-MBP.fios-router.home>
2023-11-02 21:26:31 -0400

    speakers changes

--- client/components/Products/Speakers.jsx
+++ client/components/Products/Speakers.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
+
 // component imports
 
 
 // MUI imports
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Button } from ‘mui/material’;
 
 
 const Speakers = () => {
   return (
-    <div>Speakers</div>
+    <>
+    <h1>Speakers</h1>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  </>
   )
 }
 
