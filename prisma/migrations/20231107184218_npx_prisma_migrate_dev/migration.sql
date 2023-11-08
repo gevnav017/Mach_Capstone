ef76ced6559ccba10c691b5d29145b97c974315c
Gevorg Navasardyan <gevorgnavasardyan@Enigma-Macbook.local>
2023-11-07 16:27:15 -0800

    wishlist and register  page updates

--- prisma/migrations/20231107184218_npx_prisma_migrate_dev/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "firstName" TEXT,
-ADD COLUMN     "lastName" TEXT;
--- server/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
