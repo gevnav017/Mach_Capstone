6d839325aa99332546fff82b5835ea2128d606c5
Courtney Cho <courtneycho@Courtneys-MBP.fios-router.home>
2023-11-19 12:38:43 -0500

    working on snackbar

--- client/components/Products/Speakers.jsx
+++ client/components/Products/Speakers.jsx
@@ -4,6 +4,7 @@ import Axios from "axios";
 
 // component imports
 import useCurrentUser from "../CurrentUser";
+import SimpleSnackbar from "../SimpleSnackbar";
 
 // MUI imports
 import {
@@ -25,7 +26,206 @@ import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import IconButton from "@mui/material/IconButton";
 
-const ItemsCard = ({ item, user }) => {
+// const ItemsCard = ({ item, user }) => {
+//   const [count, setCount] = useState(1);
+
+//   const decrementQty = () => {
+//     setCount((prevCount) => prevCount > 1 && prevCount - 1);
+//   };
+
+//   const addToWishlist = (speakerId) => {
+//     if (user) {
+//       Axios.post(
+//         "http://localhost:3000/api/account/wishlist",
+//         {
+//           userId: user.id,
+//           productId: speakerId,
+//         },
+//         {
+//           headers: {
+//             "content-type": "application/JSON",
+//           },
+//         }
+//       )
+//         .then((res) => console.log(res))
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     } else {
+//       console.log("no user");
+//     }
+//   };
+
+//   const addToCart = (speakerId) => {
+//     if (user) {
+//       Axios.post(
+//         "http://localhost:3000/api/account/orders",
+//         {
+//           userId: user.id,
+//           productId: speakerId,
+//           quantity: count,
+//         },
+//         {
+//           headers: {
+//             "content-type": "application/JSON",
+//           },
+//         }
+//       )
+//         .then((res) => console.log(res))
+//         .catch((err) => {
+//           console.log(err);
+//         });
+
+//     } else {
+//       console.log("no user")
+//     }
+//   };
+
+//   const navigate = useNavigate();
+
+//   const handleItemDetails = (itemId) => {
+//     // sends with params to receive by details API
+//     navigate(`/speakers/speaker-details/${itemId}`);
+//   };
+
+//   return (
+//     <Grid item xs={12} sm={6} md={4}>
+//       <Card sx={{ borderRadius: "10px" }}>
+//         <Box sx={{ width: "100%", height: 240, boxSizing: "border-box" }}>
+//           <CardMedia
+//             component="img"
+//             sx={{
+//               width: "100%",
+//               height: "100%",
+//               p: 1,
+//               objectFit: "contain",
+//               boxSizing: "border-box",
+//             }}
+//             className="productCardImg"
+//             image={item.image}
+//             title={`${item.name} ${item.type}`}
+//             onClick={() => handleItemDetails(item.id)}
+//           />
+//         </Box>
+//         <CardContent>
+//           <Typography gutterBottom variant="h5" component="div">
+//             {item.name}
+//           </Typography>
+//           <Typography
+//             gutterBottom
+//             variant="h6"
+//             color="text.secondary"
+//             component="div"
+//           >
+//             {item.brand}
+//           </Typography>
+//           <Typography gutterBottom variant="body2" color="text.secondary">
+//             {item.type}
+//           </Typography>
+
+//           <Box
+//             sx={{
+//               display: "flex",
+//               justifyContent: "space-between",
+//               alignItems: "center",
+//               gap: 2,
+//               pt: 4,
+//               mb: 2,
+//               borderTop: "1px solid",
+//               borderColor: "background.main",
+//             }}
+//           >
+//             <Typography gutterBottom variant="h6" component="div">
+//               ${item.price}
+//             </Typography>
+//             <Box
+//               sx={{
+//                 display: "flex",
+//                 alignItems: "center",
+//                 gap: 1,
+//               }}
+//             >
+//               <IconButton
+//                 onClick={decrementQty}
+//                 disabled={count === 1}
+//                 sx={{ color: "primary.main" }}
+//               >
+//                 <RemoveOutlinedIcon />
+//               </IconButton>
+//               <Typography color="text.secondary">{count}</Typography>
+//               <IconButton
+//                 onClick={() => setCount((c) => c + 1)}
+//                 sx={{ color: "primary.main" }}
+//               >
+//                 <AddOutlinedIcon />
+//               </IconButton>
+//             </Box>
+//           </Box>
+//         </CardContent>
+//         <CardActions>
+//           <Checkbox
+//             icon={<FavoriteBorder />}
+//             checkedIcon={<Favorite />}
+//             checked={item.orders[0] && item.orders[0].inWishlist}
+//             color="error"
+//             sx={{ mr: "auto" }}
+//             onClick={() => addToWishlist(item.id)}
+//           />
+//           <Button
+//             variant="contained"
+//             size="small"
+//             onClick={() => addToCart(item.id)}
+//           >
+//             <ShoppingCartOutlinedIcon size="small" sx={{ mr: 1 }} />
+//             Add to cart
+//           </Button>
+//         </CardActions>
+//       </Card>
+//     </Grid>
+//   );
+// };
+
+// const Speakers = ({ user }) => {
+//   const [speakers, setSpeakers] = useState([]);
+
+//   useEffect(() => {
+//     Axios.post(
+//       "http://localhost:3000/api/products",
+//       {
+//         // userId: user && user.id,
+//         category: "Speaker",
+//       },
+//       {
+//         headers: {
+//           "Content-Type": "application/JSON",
+//         },
+//       }
+//     )
+//       .then((res) => setSpeakers(res.data))
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, [user]);
+
+//   return (
+//     <Container maxWidth="lg" sx={{ minWidth: "400px", p: 3 }}>
+//       <Typography variant="h5" sx={{ my: 2 }}>
+//         Speakers
+//       </Typography>
+//       <Grid container spacing={2}>
+//         {speakers &&
+//           speakers.map((speaker) => (
+//             <ItemsCard key={speaker.id} item={speaker} user={user} />
+//           ))}
+//       </Grid>
+//     </Container>
+//   );
+// };
+
+// export default Speakers;
+
+
+const ItemsCard = ({ item, user, onAddToCart }) => {
   const [count, setCount] = useState(1);
 
   const decrementQty = () => {
@@ -70,7 +270,9 @@ const ItemsCard = ({ item, user }) => {
           },
         }
       )
-        .then((res) => console.log(res))
+        .then((res) => console.log(res));
+        onAddToCart()
+
         .catch((err) => {
           console.log(err);
         });
@@ -186,12 +388,24 @@ const ItemsCard = ({ item, user }) => {
 
 const Speakers = ({ user }) => {
   const [speakers, setSpeakers] = useState([]);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+
+  const handleSnackbarOpen = () => {
+    setIsSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setIsSnackbarOpen(false);
+  };
 
   useEffect(() => {
     Axios.post(
       "http://localhost:3000/api/products",
       {
-        userId: user && user.id,
+        // userId: user && user.id,
         category: "Speaker",
       },
       {
@@ -214,9 +428,10 @@ const Speakers = ({ user }) => {
       <Grid container spacing={2}>
         {speakers &&
           speakers.map((speaker) => (
-            <ItemsCard key={speaker.id} item={speaker} user={user} />
+            <ItemsCard key={speaker.id} item={speaker} onAddToCart={handleSnackbarOpen} user={user} />
           ))}
       </Grid>
+      {isSnackbarOpen && <SimpleSnackbar onClose={handleSnackbarClose} />}
     </Container>
   );
 };
