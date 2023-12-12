8d0f06122ea56e389bcf1af526b61a3e93466591
Gev <gevnav@gmail.com>
2023-12-11 22:31:40 -0800

    order

--- client/components/Account/Orders.jsx
+++ client/components/Account/Orders.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+
 // component imports
 
 // MUI imports
@@ -14,24 +15,25 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Card from "@mui/material/Card"
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from "@mui/material/Card";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const BasicSelect = () => {
-  const [ordersPlaced, setOrdersPlaced] = React.useState("");
+  const [ordersPlaced, setOrdersPlaced] = useState("");
 
   const handleChange = (event) => {
     setOrdersPlaced(event.target.value);
+
+    Axios.get("", {});
   };
 
   return (
     <Box
       sx={{
-        // minWidth: 120
-        width: "250px",
+        minWidth: "250px",
       }}
     >
       <FormControl fullWidth>
@@ -53,8 +55,9 @@ const BasicSelect = () => {
   );
 };
 
+const OrderDetails = ({price, quantity}) => {
+  console.log(price, quantity)
 
-const BasicAccordion = () => {
   return (
     <div>
       <Accordion>
@@ -63,51 +66,38 @@ const BasicAccordion = () => {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography variant="h8" style={{color: '#2998e2', fontWeight: 'bold'}}>See Order Details</Typography>
+          <Typography
+            variant="h8"
+            style={{ color: "#2998e2", fontWeight: "bold" }}
+          >
+            See Order Details
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <Typography>Quantity: {quantity}</Typography>
+          <Typography>Price: ${price}</Typography>
+          <Typography>Total: ${price * quantity}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
   );
-}
-
+};
 
 const Orders = ({ user }) => {
   // state to hold orders
   const [orders, setOrders] = useState([]);
+  console.log(orders)
 
   // state to track whether orders are loading
   const [loading, setLoading] = useState(true);
 
   // axios call to get orders that have inCart column false by logged in user from db
   useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const userId = user.id;
-        const response = await Axios.get(
-          `http://localhost:3000/api/orders/archive/${userId}`
-        );
-          console.log(userId)
-        setOrders(response.data);
-        setLoading(false);
-        console.log(response)
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-      }
-    };
-
-    fetchOrders();
+    const userId = user && user.id;
+    Axios.get(`http://localhost:3000/api/all-orders/${userId}`)
+      .then((res) => setOrders(res.data))
+      .then(setLoading(false));
   }, [user.id]);
-
-  // function to add view order details
-  // const viewOrderDetails = (orderId) => {
-  //   console.log(orderId);
-  // };
 
   return (
     <Container maxWidth="lg" sx={{ p: 3 }}>
@@ -128,52 +118,60 @@ const Orders = ({ user }) => {
       {loading ? (
         <Typography variant="h6">Loading...</Typography>
       ) : orders.length === 0 ? (
-        <Typography variant="h6" style={{
-          display: 'flex',
-          justifyContent: 'center'
-        }}>NO ORDER HISTORY AVAILABLE</Typography>
+        <Typography
+          variant="h6"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          NO ORDER HISTORY AVAILABLE
+        </Typography>
       ) : (
-      <Grid
-        container
-        spacing={2}
-        style={{
-          // background: 'linear-gradient(-225deg, #5D9FFF 0%, #2998e2 45%, #e2e7ec 100%)',
-          display: "flex",
-          flexDirection: "column",
+        <Grid
+          container
+          spacing={2}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {/* map through orders state to display all orders from the logged in user */}
+          {/* button on each order item to click and view order details...dialog box opens up */}
+          {/* Grid to display orders as cards */}
 
-        }}
-      >
-        {/* map through orders state to display all orders from the logged in user */}
-      {/* button on each order item to click and view order details...dialog box opens up */}
-      {/* Grid to display orders as cards */}
-
-        {orders.map((order) => (
+          {orders.map((order) => (
             // <Grid item key={order.id} xs={12} md={5} lg={4}>
             <Grid item key={order.id}>
               {/* <Card sx={{ height: "100%" }}> */}
-              <Card style={{
-                maxWidth: '98%',
-                marginBottom: '16px'
-              }}>
-                <CardContent style={{
-                  display: "flex",
-                  flexDirection: "column"
-                }}>
+              <Card
+                style={{
+                  maxWidth: "98%",
+                  marginBottom: "16px",
+                }}
+              >
+                <CardContent
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
                   {/* Display order info */}
                   <Typography variant="h7" gutterBottom>
-                    Order Date: {order.dateOrdered.slice(0,10)}
+                    Order Date: {order && order.dateUpdated.slice(0, 10)}
                   </Typography>
                   <Typography variant="h7" gutterBottom>
-                    {/* Order #: {order.id} */}
-                    Order #: {order.id.slice(0,8)}
+                    Order #: {order && order.id.slice(0, 8)}
                   </Typography>
-                  <Typography variant="h7" gutterBottom style={{marginBottom: '10px'}}>
+                  <Typography
+                    variant="h7"
+                    gutterBottom
+                    style={{ marginBottom: "10px" }}
+                  >
                     Total: ${order.total}
                   </Typography>
                   {/* To show items in order summary */}
-                  <BasicAccordion style={{
-              
-                  }}/>
+                  <OrderDetails price={order.products.price} quantity={order.quantity} />
                   {/* Displaying images and product names */}
                   {/* {order.items.map((item) => (
                     <div
@@ -205,14 +203,11 @@ const Orders = ({ user }) => {
                 </CardContent>
               </Card>
             </Grid>
-          ))}     
-      </Grid>
+          ))}
+        </Grid>
       )}
     </Container>
   );
 };
 
 export default Orders;
-
-
-
