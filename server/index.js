2264eb5a4a7b354ad4f6d29acfb2dbc5dea89f18
Gev <gevnav@gmail.com>
2023-12-14 19:39:08 -0800

    deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
